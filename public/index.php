d01144ddfb5f5b5bd538f957ba29829cacc07b30
Nikolay Kyznetsov <kyznetsovnic@gmail.com>
2021-01-25 16:54:40 +0300

    Added request class

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
-$name = $_GET['name'] ?? 'Guest';
+chdir(dirname(__DIR__));
+require_once 'vendor/autoload.php';
 
+use Framework\Http\Request;
+
+### Initialization
+
+$request = new Request();
+
+### Action
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+header('X-Developer: Kyznetsov');
+
+$name = $_GET['name'] ?? 'Guest';
 echo "Hello $name";
