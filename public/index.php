1af7ac617b14eaf6209971ced171082c7327e793
Nikolay Kyznetsov <kyznetsovnic@gmail.com>
2021-01-26 14:00:26 +0300

    Install Zend Diactoros.

--- public/index.php
+++ public/index.php
@@ -3,18 +3,18 @@
 chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
 
-use Framework\Http\RequestFactory;
-use Framework\Http\Response;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
 
 ### Initialization
 
-$request = RequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 ### Action
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$response = (new Response('Hello, ' . $name . '!'))
+$response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'Kyznetsov');
 
 ### Sending
